0433532d1e466f73f900ef88a1a2960582f54c1a
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2023-12-21 13:18:34 -0300

    agregado estilo

--- src/app/components/exchange/exchange.component.ts
+++ src/app/components/exchange/exchange.component.ts
@@ -16,20 +16,26 @@ export class ExchangeComponent {
 
   conversionService = inject(ConversionService)
   quantity:number = 0
-@Input() fromconversion:FromConversion  =
-{
-  currencySymbol:''
-  
-}
-@Input() toconversion:ToConversion =
-{
-  currencySymbol:''
-}
+  @Input() fromconversion:FromConversion  = { currencySymbol:'' };
+  @Input() toconversion:ToConversion = { currencySymbol:'' };
+async exchange() {
+  try {
+    const res = await this.conversionService.conversion(
+      this.fromconversion,
+      this.toconversion,
+      this.quantity
+  );
+      console.log('From Conversion:', this.fromconversion.currencySymbol);
+      console.log('To Conversion:', this.toconversion.currencySymbol);
+      console.log(res); // Imprime la respuesta en la consola
+      // Resto del código...
+  } catch (error) {
+      console.error('Error en la conversión:', error);
+      console.log('From Conversion:', this.fromconversion.currencySymbol);
+      console.log('To Conversion:', this.toconversion.currencySymbol);
 
-async exchange()
-{
-const res = await this.conversionService.conversion(this.fromconversion,this.toconversion,this.quantity)
+      // Puedes mostrar un mensaje de error al usuario si es necesario
+  }
 }
-
 }
 
--- src/app/services/conversion.service.ts
+++ src/app/services/conversion.service.ts
@@ -11,7 +11,7 @@ export class ConversionService extends ApiService
 {
     async conversion(fromconversion:FromConversion,toconversion:ToConversion,quantity:number): Promise<{ conversionRate: number }> {
         try {
-          const res = await fetch(API + 'Currency/convertir-moneda', {
+          const res = await fetch(API + 'Currency/', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
@@ -19,15 +19,25 @@ export class ConversionService extends ApiService
             },
             body: JSON.stringify({
               symbol1: fromconversion.currencySymbol,
-              symbol2: toconversion.currencySymbol, // Corregido
+              symbol2: toconversion.currencySymbol,
               quantity
-            })
+          })
           });
           if (res.ok) {
             const conversionData = await res.json(); // Parsea la respuesta JSON
             console.log(conversionData)
+            console.log('From Conversion:', fromconversion);
+            console.log('To Conversion:', toconversion);
+            console.log( "devuelve "+JSON.stringify({
+              symbol1: fromconversion.currencySymbol,
+              symbol2: toconversion.currencySymbol,
+              quantity
+          }));
             return conversionData; // Devuelve el objeto con la tasa de conversión
           } else {
+            console.log('From Conversion:', fromconversion);
+            console.log('To Conversion:', toconversion);
+
             throw new Error('Error al convertir moneda');
           }
         } catch (error) {
