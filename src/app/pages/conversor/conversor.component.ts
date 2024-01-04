56b3d6bf50dcf3127b98c97cfbb39e025be494d2
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-01-04 12:53:57 -0300

    design implemented

--- src/app/pages/conversor/conversor.component.ts
+++ src/app/pages/conversor/conversor.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, HostListener, inject } from '@angular/core';
 import { CurrencyService } from 'src/app/services/currency.service';
 import { Currency } from 'src/app/interfaces/currency';
+
 @Component({
   selector: 'app-conversor',
   templateUrl: './conversor.component.html',
   styleUrls: ['./conversor.component.scss']
 })
 export class ConversorComponent implements OnInit {
-  
+
   currencyService = inject(CurrencyService);
-  currencies:Currency[] = []
+  currencies: Currency[] = [];
+  textoAModoMobile = '';
+
+  isMobileResolution(): boolean {
+    return window.innerWidth <= 768;
+  }
 
   ngOnInit(): void {
     this.currencyService.getAll().then(res => {
       this.currencies = res;
       console.log("Lista de monedas:", res);
     });
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    if (!this.isMobileResolution()) {
+      this.textoAModoMobile = '';
+    }
   }
 }
