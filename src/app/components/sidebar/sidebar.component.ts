2e1f2997a83c8fe8786bab29c84aa346a95f4c65
Sebas <Rojassebastian80972a@gmail.com>
2024-02-17 23:13:15 -0300

    more style

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -4,6 +4,7 @@ import { Router, RouterModule } from '@angular/router';
 import { Profile } from 'src/app/interfaces/user';
 import { ProfileIconComponent } from "../profile-icon/profile-icon.component";
 import { ProfileService } from 'src/app/services/profile.service';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
     selector: 'app-sidebar',
@@ -12,7 +13,12 @@ import { ProfileService } from 'src/app/services/profile.service';
     styleUrls: ['./sidebar.component.scss'],
     imports: [CommonModule, RouterModule, ProfileIconComponent]
 })
-export class SidebarComponent {
+export class SidebarComponent extends ApiService {
+    router = inject(Router);
+      logout(){
+        this.auth.logout();
+      }
+  
     isAdmin: boolean = true;
     profileService = inject(ProfileService);
 
